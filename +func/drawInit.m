76b6ad4f83f0ee02d15e85c35684ba7e7b37d6d4
西野択 <nishino.t.ae@m.titech.ac.jp>
2024-03-27 07:18:52 +0900

    ライントレース関連のファイルをアーカイブ

--- +func/drawInit.m
+++
@@ -1,5 +0,0 @@
-function drawInit(ax, field_size)
-xlim(ax, [0 field_size(1,1)])
-ylim(ax, [0 field_size(1,2)])
-daspect(ax, [1 1 1]);
-end
--- +func/wrapPi.m
+++
@@ -1,9 +0,0 @@
-function y = wrapPi(x)
-if x > pi
-    y = x - pi*floor(x/pi);
-elseif x < -pi
-    y = x + pi*floor(x/-pi);
-else
-    y = x;
-end
-end
